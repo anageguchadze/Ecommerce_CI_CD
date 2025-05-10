cf5249babcbaf67ba97a49a5a1390f1c58201610
Ana <anageguchadze22@gmail.com>
2025-05-10 16:49:18 +0400

    Add github workflows

--- ecommerce_pro/settings.py
+++ ecommerce_pro/settings.py
@@ -75,10 +75,21 @@ WSGI_APPLICATION = 'ecommerce_pro.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ecommerce_db',
+        'USER': 'postgres',
+        'PASSWORD': 'postgresadmin22',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
 
