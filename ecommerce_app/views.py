dc3871361418c086e3e15c7be21cf1d8659b29a0
Ana <anageguchadze22@gmail.com>
2025-05-10 15:05:21 +0400

    Add viewsets

--- ecommerce_app/views.py
+++ ecommerce_app/views.py
@@ -1,5 +1,34 @@
 from rest_framework import viewsets
 from .models import Product, Order, OrderItem
 from .serializers import ProductSerializer, OrderSerializer, OrderItemSerializer
+from rest_framework.permissions import IsAuthenticated, AllowAny
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [AllowAny]
+    
+
+class OrderViewSet(viewsets.ModelViewSet):
+    serializer_class = OrderSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Order.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
 
 
+class OrderItemViewSet(viewsets.ModelViewSet):
+    serializer_class = OrderItemSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return OrderItem.objects.filter(order__user=self.request.user)
+
+    def perform_create(self, serializer):
+        order = serializer.validated_data['order']
+        if order.user != self.request.user:
+            raise PermissionError("You do not have permission to add items to this order.")
+        serializer.save()
